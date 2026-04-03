ee50a7932c5e7b40fc8fd2b87303d34b83c00d2e
axxuetam1 <axxuetam@gmail.com>
2026-04-03 14:51:33 +0500

    Разработанные переменные. Климов

--- app/src/main/java/com/example/gps_klimov/MainActivity.java
+++ app/src/main/java/com/example/gps_klimov/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.gps_klimov;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,15 +15,42 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    LocationManager _LocationManager;
+    int ACCESS_FINE_LOCATION;
+    int ACCESS_COARSE_LOCATION;
+    TextView result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        result = findViewById(R.id.text);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    LocationListener _LocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            if (location == null) return;
+            else {
+                String message = "";
+                if (location.getProvider().equals(LocationManager.GPS_PROVIDER)) {
+                    message += "\nМестоположение определено с помощью GPS: долгота - " +
+                            location.getLatitude() + " широта - " + location.getLatitude();
+                }
+                if (location.getProvider().equals(LocationManager.NETWORK_PROVIDER)) {
+                    message += "\nМестоположение определено с помощью интернета: долгота - " +
+                            location.getLatitude() + " широта - " + location.getLatitude();
+                }
+                result.setText(message);
+            }
+        }
+    };
 }
